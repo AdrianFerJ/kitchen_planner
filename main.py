1635357a4f993e23cc302eb97ae6758970cf741a
adrian <adrianfj@gmail.com>
2019-04-02 13:09:30 -0400

    Created a function to save scrapping results

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ def get_this_recipe(url):
     recipe_info = scrapper(url, soup)
 
     if recipe_info:
-        print("# Sucess!")
+        print("# Recipe scrapped!")
         pretty_data = json.dumps(recipe_info, indent=4)
         print(pretty_data)
-        return recipe_info
+        hp.add_recipe_to_book(recipe_info)
     else:
         print("Error!")
 
@@ -35,9 +35,11 @@ def get_this_recipe(url):
 """
     Run Script
 """
-# Recipe url (Feel free to change the test URL)
-TEST_URL = 'https://www.allrecipes.com/recipe/257938/spicy-thai-basil-chicken-pad-krapow-gai/?clickId=right%20rail1&internalSource=rr_feed_recipe_sb&referringId=60598%20referringContentType%3Drecipe'
-#'https://www.allrecipes.com/recipe/60598/vegetarian-korma/?internalSource=hub%20recipe&referringContentType=Search'
 
-# Request recipe! 
-get_this_recipe(TEST_URL)
+if __name__ == '__main__':
+    # Recipe url (Feel free to change the test URL)
+    TEST_URL = 'https://www.allrecipes.com/recipe/257938/spicy-thai-basil-chicken-pad-krapow-gai/?clickId=right%20rail1&internalSource=rr_feed_recipe_sb&referringId=60598%20referringContentType%3Drecipe'
+    #'https://www.allrecipes.com/recipe/60598/vegetarian-korma/?internalSource=hub%20recipe&referringContentType=Search'
+
+    # Request recipe! 
+    get_this_recipe(TEST_URL)
